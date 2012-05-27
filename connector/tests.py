d347be7e2de0db88d0ac263d282efa65e1973c0f
Chris Martel <chris@codeways.org>
2012-05-27 18:22:37 +0200

    Added team info connector w/ tests.

--- connector/tests.py
+++ connector/tests.py
@@ -1,5 +1,5 @@
 from django.utils import unittest
-from connector.footballpool import FootballPool
+from connector.models import FootballPool
 
 class FootballPoolTestCase(unittest.TestCase):
     def setUp(self):
@@ -11,17 +11,17 @@ class FootballPoolTestCase(unittest.TestCase):
         self.assertEqual(self.fp.fetch_team_item(7, 'country'), 'Germany')
         self.assertEqual(self.fp.fetch_team_item(7, 'country_flag'), 'http://footballpool.dataaccess.eu/images/flags/de.png')
         self.assertEqual(self.fp.fetch_team_item(7, 'info_url'), 'http://en.wikipedia.org/wiki/Germany_national_football_team')
-        self.assertEqual(self.fp.fetch_team_item(7, 'coach_name'), 'Joachim Löw')
+        self.assertEqual(self.fp.fetch_team_item(7, 'coach_name'), u'Joachim L\xf6w')
 
         self.assertEqual(self.fp.fetch_team_item(7, 'name'), 'Germany')
         self.assertEqual(self.fp.fetch_team_item(7, 'country'), 'Germany')
         self.assertEqual(self.fp.fetch_team_item(7, 'country_flag'), 'http://footballpool.dataaccess.eu/images/flags/de.png')
         self.assertEqual(self.fp.fetch_team_item(7, 'info_url'), 'http://en.wikipedia.org/wiki/Germany_national_football_team')
-        self.assertEqual(self.fp.fetch_team_item(7, 'coach_name'), 'Joachim Löw')
+        self.assertEqual(self.fp.fetch_team_item(7, 'coach_name'), u'Joachim L\xf6w')
 
         self.assertEqual(self.fp.fetch_team_item(9, 'name'), 'Spain')
         self.assertEqual(self.fp.fetch_team_item(9, 'country'), 'Spain')
         self.assertEqual(self.fp.fetch_team_item(9, 'country_flag'), 'http://footballpool.dataaccess.eu/images/flags/es.png')
         self.assertEqual(self.fp.fetch_team_item(9, 'info_url'), 'http://en.wikipedia.org/wiki/Spain_national_football_team')
-        self.assertEqual(self.fp.fetch_team_item(9, 'coach_name'), 'Vicente del Bosque')
+        self.assertEqual(self.fp.fetch_team_item(9, 'coach_name'), u'Vicente del Bosque')
 
